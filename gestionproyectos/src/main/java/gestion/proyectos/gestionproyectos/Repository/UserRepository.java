5431f7e1424d44d029ec20abb169dd536d40a9f5
Jaime Riquelme <jaime.riquelme@usach.cl>
2024-12-14 11:50:58 -0300

    Init JWT implementation

--- gestionproyectos/src/main/java/gestion/proyectos/gestionproyectos/Repository/UserRepository.java
+++ gestionproyectos/src/main/java/gestion/proyectos/gestionproyectos/Repository/UserRepository.java
@@ -4,6 +4,10 @@ import gestion.proyectos.gestionproyectos.Entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+    Boolean existsByEmail(String email);
 }
