3fdc6c5df28534ce68609a303c1eef02e7d66e80
Ch3chS <sergio.espinoza.g@usach.cl>
2024-12-21 16:54:33 -0300

    Fix Parameter Problems Related to #39

--- gestionproyectos/src/main/java/gestion/proyectos/gestionproyectos/Entity/Parameter.java
+++ gestionproyectos/src/main/java/gestion/proyectos/gestionproyectos/Entity/Parameter.java
@@ -2,6 +2,7 @@ package gestion.proyectos.gestionproyectos.Entity;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
@@ -14,19 +15,35 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Data
 public class Parameter {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id_parameter")
     private Long idParameter;
 
-    @JsonBackReference
+    @JsonBackReference // Evita la serialización de la relación para evitar ciclos infinitos
     @ManyToOne
     @JoinColumn(name = "exit_id")
     private Exit exit;
 
-    @Column(name = "name_parameter", unique = true)
-    private String nameParameter;  
+    @JsonProperty("idExit") // Exponer el ID del exit relacionado para permitir su uso en la serialización
+    public Long getIdExit() {
+        return exit != null ? exit.getIdExit() : null;
+    }
 
+    @JsonProperty("idExit") // Permitir seteo durante la deserialización
+    public void setIdExit(Long idExit) {
+        if (idExit != null) {
+            this.exit = new Exit(); // Crear una instancia parcial de Exit asignando solo el ID
+            this.exit.setIdExit(idExit);
+        }
+    }
+
+    @Column(name = "name_parameter", unique = true)
+    private String nameParameter;
+
+    @Column(name = "content", columnDefinition = "TEXT")
     private String content;
+
     private String state;
 }
